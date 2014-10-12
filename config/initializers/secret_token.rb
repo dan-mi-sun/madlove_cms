e98b3c0fdad0d2bd3e0e6b34b7e0a4c529d72eac
Daniel Sun <dan.mi.sun@gmail.com>
2014-10-12 13:12:42 +0100

    first commit rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MadloveCms::Application.config.secret_token = 'e3f4c7fbb160d731f7cf818c79515fe3bcb95286ab765df4d7bcde3398d478353bfde94907fc6674a88e8ddbd51f7e3750d62df28066e5682ad9f503ce317458'
