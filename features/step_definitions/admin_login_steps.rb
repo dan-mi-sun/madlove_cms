ddde0de427b227ea307f935d220e38b0b61c6221
Daniel Sun <dan.mi.sun@gmail.com>
2014-10-20 10:21:03 +0100

    add pending steps for admin login feature

---
+++ features/step_definitions/admin_login_steps.rb
@@ -0,0 +1,20 @@
+Given(/^that an admin exists$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^that an admin is on the login page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^an admin fills in their username$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^they fill in their password$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^they should be logged in\.$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
