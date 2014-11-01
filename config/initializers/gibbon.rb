197daa98732278f404b492688dd52be4fb2d240d
Daniel Sun <dan.mi.sun@gmail.com>
2014-11-01 13:40:19 +0000

    add env variables for gibbon mailchimp API

--- config/initializers/gibbon.rb
+++ config/initializers/gibbon.rb
@@ -1,4 +1,4 @@
-Gibbon::API.api_key = "YOUR-API-KEY"
-Gibbon::API.timeout = 15
-Gibbon::API.throws_exceptions = false
+Gibbon::API.api_key = ENV["GIBBON_API_KEY"]
+Gibbon::API.timeout = ENV["GIBBON_TIMEOUT"]
+Gibbon::API.throws_exceptions = ENV["GIBBON_THROWS_EXCEPTIONS"]
 
