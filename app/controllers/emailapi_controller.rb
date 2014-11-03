d776de10fb62c870a7482260d7f39f7c25af538a
Daniel Sun <dan.mi.sun@gmail.com>
2014-11-03 09:29:48 +0000

    create subscribe method for gibbon email api

--- app/controllers/emailapi_controller.rb
+++ app/controllers/emailapi_controller.rb
@@ -1,2 +1,18 @@
 class EmailapiController < ApplicationController
+
+  def index
+  end
+
+  def subscribe
+
+    @list_id = "YOUR-LIST-ID"
+    gb = Gibbon::API.new
+
+    gb.lists.subscribe({
+      :id => @list_id,
+      :email => {:email => params[:email][:address]}
+    })
+
+  end
+
 end
